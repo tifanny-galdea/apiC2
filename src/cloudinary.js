7e0a933b00f2ce55bfd464332861546effd0302d
tifanny-galdea <tifannygm13@gmail.com>
2025-05-24 15:35:31 -0500

    API-Completa8

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -15,7 +15,10 @@ const storage = new CloudinaryStorage({
   params: {
     folder: 'productos', // carpeta en Cloudinary
     allowed_formats: ['jpg', 'png', 'jpeg'],
-    public_id: (req, file) => `${Date.now()}-${file.originalname}`
+    public_id: (req, file)  => {
+      const nombreSinExt = path.parse(file.originalname).name; // nombre sin extensión
+      return `${Date.now()}-${nombreSinExt}`;
+    }
   }
 });
 
